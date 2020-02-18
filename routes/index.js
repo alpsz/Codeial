2d6d76937f0d3b81e56bb91699252411d46aaad6
alpsz <alpesh2007202gmail.com>
2020-02-18 11:13:34 +0530

    add posts and profile controller

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+
 console.log("Router loaded");
-module.exports = router;
+
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+router.use('/userposts',require('./posts'));
+
+module.exports = router;
