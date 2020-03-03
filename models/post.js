61b6dca6fcb798b6ac2e151edf7d7408d3207241
alpsz <alpesh2007202gmail.com>
2020-03-03 11:56:13 +0530

    Commit all changes

--- models/post.js
+++ models/post.js
@@ -8,8 +8,13 @@ const postSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'
+    },
 
-    }
+    // include the array of ids of all the comments in this post schema itself
+    comments:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Comment',
+    }]
 },{
    timestamps:true 
 });
